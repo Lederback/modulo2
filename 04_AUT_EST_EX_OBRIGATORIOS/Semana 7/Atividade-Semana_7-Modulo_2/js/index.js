f7b1ba5fa9b4815c8f05a5915003d7dcbccba97e
Renato Machado <renatosmachado2@gmail.com>
2022-06-02 17:06:56 -0300

    Atualização Atividade Semana 7

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Atividade-Semana_7-Modulo_2/js/index.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Atividade-Semana_7-Modulo_2/js/index.js
@@ -1,3 +1,4 @@
+//Exercício 1
 function plusLess(type){
     let input = document.getElementById("input-ex1");
     switch(type){
@@ -10,10 +11,49 @@ function plusLess(type){
     }
 }
 
+//Exercício 2
 function change(){
     const input1 = document.getElementById("input-ex2A").value;
     const input2 = document.getElementById("input-ex2B").value;
 
     document.getElementById("input-ex2B").value = input1;
     document.getElementById("input-ex2A").value = input2;
+}
+
+//Exercício 3
+function numberValidation(){
+    let pattern = /^\([1-9]{2}\)(?:[2-8]|[1-9][1-9])[0-9]{3}\-[0-9]{4}$/g;
+    let text = document.getElementById("input-tel").value;
+
+    if(text.match(pattern) != null){
+        document.getElementById("result-validation").innerHTML = "Telefone escrito corretamento";
+    }
+    else{
+        document.getElementById("result-validation").innerHTML = "Telefone ecrito no formato errado, tente este (XX)XXXXX-XXXX";
+    }
+}
+
+//Exercício 4
+function travel(){
+    const qtdTravelers = document.getElementById("input-travelers").value;
+    const period = document.getElementById("select-period").value;
+
+    switch(period){
+        case "Diurno":
+            if(qtdTravelers > 50){
+                document.getElementById("total-travel").innerHTML = (qtdTravelers * 200) * 0.6;
+            }
+            else{
+                document.getElementById("total-travel").innerHTML = qtdTravelers * 200;
+            }
+            break;
+        case "Noturno":
+            if(qtdTravelers > 50){
+                document.getElementById("total-travel").innerHTML = (qtdTravelers * 100) * 0.8;
+            }
+            else {
+                document.getElementById("total-travel").innerHTML = qtdTravelers * 100
+            }
+            break;
+    }
 }
